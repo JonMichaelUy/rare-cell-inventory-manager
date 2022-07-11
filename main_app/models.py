5138597e50eabc7487111590746bb6d9ecf3664e
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-07-11 13:41:49 -0400

    wip: setup index page and ininitial db model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+TESTED = (
+    ('P', 'Pos'),
+    ('N', 'Neg'),
+    ('?', 'NT'),
+)
 # Create your models here.
+
+
+class Unit(models.Model):
+    unit_id = models.CharField(max_length=20)
+    ABO = models.CharField(max_length=2)
+    D = models.CharField(
+        max_length=1,
+        choices=TESTED,
+        default=TESTED[1][1]
+    )
+    location = models.CharField(max_length=20)
+    shelf = models.IntegerField()
